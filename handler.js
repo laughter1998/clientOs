0f5c83eab632ad2f18f985fae57d8cbad3b0a812
laughter1998 <laughter98@hanmail.net>
2022-08-04 10:45:35 +0900

    first

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+const os = require('os');
+
+module.exports.getOsInfo = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        osName: os.hostname(),
+        osPlatform: os.platform(),
+        osCPU: os.cpus(),
+        // 1mb = 1048576
+        // hi
+        osMemory: os.totalmem() / 1048576,
+      },
+      null,
+      2
+    ),
+  };
+};
